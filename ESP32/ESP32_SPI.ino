76852969da0798381541bb9107400ba91b07332e
nicolas-le-petit <52048222+nicolas-le-petit@users.noreply.github.com>
2021-02-25 16:40:45 +0700

    Update ESP32_SPI.ino
    fixed bug send double bytes + clk -> send (MOSI) ok, receive (MISO) fail???
    clk = 5MHz ok -> 10MHz fail???

--- ESP32/ESP32_SPI.ino
+++ ESP32/ESP32_SPI.ino
@@ -1,18 +1,34 @@
 /**
- * ESP32 pinout:
- * We need 04 pins (wires) to communicate btw esp32 & FPGA
- * ssFPGA    5// pin selects slave ID
- * MOSI      23// 
- * MISO      19//
- * SCK       18//
+We need 04 pins (wires) to communicate btw esp32 & FPGA
+The default pins for SPI on the ESP32.
+HSPI
+MOSI = GPIO13
+MISO = GPIO12
+CLK = GPIO14
+CD = GPIO15
+
+/SPI interface
+  .i_SPI_Clk (GPIO_0[2]),
+  .o_SPI_MISO(GPIO_1[0]),
+  .i_SPI_MOSI(GPIO_0[0]),
+  .i_SPI_CS_n(GPIO_0[1])
+  
+VSPI
+MOSI = GPIO23 --> GPIO0 - 0
+MISO = GPIO19 --> GPIO1 - 0
+CLK/SCK = GPIO18 --> GPIO0 - 2
+CS/SS = GPIO5 --> GPIO0 - 1
  **/
 
 #include <SPI.h>
 
-uint8_t const ssFPGA = 5;
-uint8_t const maxSpeed = 50000000;
-#define Set_CS 0 //LOW is selected
-#define Reset_CS 1 //
+#define ssFPGA 5// 5 pin selects slave ID
+#define MOSI 23// 
+#define MISO 19//
+#define SCK 18//  18
+//uint32_t const maxSpeed = 5000000;
+#define Set_CS LOW //LOW is selected
+#define Reset_CS HIGH //
 
 void setup()
 {
@@ -22,34 +38,37 @@ void setup()
     }
     Serial.println("Serial Begin");
     
-    SPI.begin();   
+    SPI.begin(SCK, MISO, MOSI, ssFPGA);   
     Serial.println("SPI Begin");   
     
     pinMode(ssFPGA, OUTPUT); 
+    digitalWrite(ssFPGA, Reset_CS);
     Serial.println("Please type input: 0 or 1");   
 }
 
 void loop() 
-{
-    while (!Serial.available()){
-      ;
-    }
-    byte mosi = Serial.read();
-    delay(10);
-    SPI.beginTransaction(SPISettings(maxSpeed, MSBFIRST, SPI_MODE0)); //config SPI transaction with 03 parametter: maxSpeed/ dataOrder/ dataMode
-                                                                          //need to be adapted with FPGA KIT
-    digitalWrite(ssFPGA, Set_CS);//select slave
-    uint8_t miso = SPI.transfer(mosi);
-    Serial.print("SPI transfered to FPGA: ");
-    Serial.println(mosi, HEX);
-    Serial.print("Data received from FPGA: ");
-    Serial.println(miso, HEX);
-    digitalWrite(ssFPGA, Reset_CS);
-    SPI.endTransaction();
-    
-//
-//  static int ii = 0;
-//  if (++ii % 1024 == 0) {
-//    Serial.print(".");  // show a sign of life
-//  }
+{     
+    //Wait & read data from Serial
+      while (!Serial.available()){
+        ;
+      }
+      byte mosi = Serial.read();
+      Serial.flush();
+      delay(10);
+        
+      SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0)); //config SPI transaction with 03 parametter: maxSpeed/ dataOrder/ dataMode
+                                                                            //need to be adapted with FPGA KIT
+    //SPI.setFrequency(maxSpeed);
+    //Start transfer via SPI
+      digitalWrite(ssFPGA, Set_CS);//select slave
+      byte miso = SPI.transfer(mosi);
+      digitalWrite(ssFPGA, Reset_CS);
+      SPI.endTransaction();
+      
+    //Console
+//      Serial.print("SPI transfered to FPGA: ");
+//      Serial.println(mosi, HEX);
+//      Serial.print("Data received from FPGA: ");
+      Serial.println(miso, HEX);
+      Serial.flush();
 }
